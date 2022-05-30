452c739f24729cc04513b91d1543e36c4aea9f24
Patryk Wiśniewski <pwisniewski@infoprojekt.pl>
2022-05-30 15:08:02 +0200

    setup bootstrap

--- csgoroll/src/app/app.module.ts
+++ csgoroll/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GraphQLModule } from './graphql.module';
+import { GraphQLModule } from './graphql/graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
