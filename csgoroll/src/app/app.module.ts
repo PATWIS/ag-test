dac454c2646e1f865030d159f8fe2ab4584fb864
Patryk Wiśniewski <pwisniewski@infoprojekt.pl>
2022-05-30 15:10:19 +0200

    init header component

--- csgoroll/src/app/app.module.ts
+++ csgoroll/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql/graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, GraphQLModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
